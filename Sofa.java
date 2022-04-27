ada18cf0da2ebbad6fbcfc48170c697f1c7402ee
Tiago Camillo <66873418+tcamillo4-ctrlz@users.noreply.github.com>
2022-04-26 22:16:30 -0300

    Resolução exercício SOFA prova

---
+++ Sofa.java
@@ -0,0 +1,57 @@
+public class Sofa{
+
+    String cor;
+    String tamanho;
+    boolean reclinado;
+
+    public Sofa(String cor, String tamanho, boolean reclinado){
+        this.cor = cor;
+        this.tamanho = tamanho;
+        this.reclinado = reclinado;
+    }
+
+    public void setCor(String novacor) {
+        cor = novacor;
+    }
+    public String getCor() {
+        return cor;  
+    }
+
+    public void setTamanho(String novotamanho) {
+        tamanho = novotamanho;
+    }
+    public String getTamanho() {
+        return tamanho;  
+    }
+
+    public void setReclinado(String novoreclinado) {
+        reclinado = novoreclinado;
+    }
+    public String getTecido() {
+        return reclinado;  
+    }
+
+    public void reclinarencosto(){
+        System.out.println("Sofa reclinado!");
+        this.reclinado = true;
+    }
+
+    public void voltarencosto(){
+        System.out.println("Sofa voltou ao normal!");
+        this.reclinado = false;
+    }
+
+    public static void main(String[] args) {
+        
+        Sofa sofa = new Sofa();
+
+        sofa.reclinarencosto();
+
+        sofa.voltarencosto();
+
+    }
+
+
+
+
+}
